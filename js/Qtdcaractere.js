d40f1595618f66fde194ace6fde23171aa02dc4b
NycolasSF <nycolassilvafroes@gmail.com>
2018-04-16 22:48:11 -0400

    Adicionei um modal e tirei alguns erros

---
+++ js/Qtdcaractere.js
@@ -0,0 +1,13 @@
+var myText = document.getElementById("comment");
+var wordCount = document.getElementById("mostrarCaractere");
+myText.addEventListener("keyup",function(){
+	var characters = myText.value.split('');
+  wordCount.innerText = characters.length;
+  if(characters.length > 50){
+  	myText.value = myText.value.substring(0,50);
+    wordCount.innerText =  50;
+    if (wordCount = 50) {
+      alert('Chegou no limite');
+    }
+  }
+});
